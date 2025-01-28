a68738a906e448b614bfeb1e5def0e8aba714fbb
chandu16 <jagadishdevu523@icloud.com>
2025-01-28 12:55:50 +0530

    data transformation added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.mlflowProject import logger
 from mlflowProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlflowProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from mlflowProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -20,6 +20,18 @@ STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
